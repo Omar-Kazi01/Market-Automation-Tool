2f76418780f69b8306926d64d08e058b82bf274a
Omar-Kazi01 <syedomarabdullahkazi@gmail.com>
2025-04-26 12:40:14 +0530

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -34,12 +34,12 @@ if ($result->num_rows > 0) {
                     $mail->isSMTP();
                     $mail->Host       = 'smtp.gmail.com';
                     $mail->SMTPAuth   = true;
-                    $mail->Username   = 'MarketAT001@gmail.com';
-                    $mail->Password   = 'itjc suug rbbx gekx';
+                    $mail->Username   = 'email';
+                    $mail->Password   = 'password';
                     $mail->SMTPSecure = 'tls';
                     $mail->Port       = 587;
 
-                    $mail->setFrom('MarketAT001@gmail.com', 'ADMIN');
+                    $mail->setFrom('email', 'ADMIN');
                     $mail->addAddress($row['email'], $row['name']);
 
                     $mail->isHTML(true);
